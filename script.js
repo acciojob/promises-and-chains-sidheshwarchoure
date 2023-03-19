1631f7a8179ee36c5f594bc3dfa8b8ed09f7b77b
sidheshwarchoure <92964594+sidheshwarchoure@users.noreply.github.com>
2023-03-19 18:27:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -31,66 +31,26 @@ btn.addEventListener('click', (event) => {
     alert(`Oh sorry ${name}. You aren't old enough.`);
   });
 });
+
 function myPromise() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve({
-        name: 'John',
-        age: 25
-      });
-    }, 4000);
-  })
-  .then(obj => {
-    return obj.age;
-  })
-  .then(age => {
-    return {
-      welcomeMessage: `Welcome, John. You are ${age} years old.`
-    };
-  });
-}
-
-myPromise().then(data => {
-  alert(data.welcomeMessage);
-});
-And here's an example of how you could use this promise to validate a form and show an alert based on the age input:
-
-javascript
-Copy code
-const form = document.querySelector('form');
-const ageInput = document.getElementById('age');
-const nameInput = document.getElementById('name');
-const btn = document.getElementById('btn');
-
-btn.addEventListener('click', (event) => {
-  event.preventDefault();
-  
-  const age = parseInt(ageInput.value);
-  const name = nameInput.value;
-  
-  if (isNaN(age) || age < 1 || name.trim() === '') {
-    alert('Please fill out both fields.');
-    return;
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          name: 'John',
+          age: 25
+        });
+      }, 4000);
+    })
+    .then(obj => {
+      return obj.age;
+    })
+    .then(age => {
+      return {
+        welcomeMessage: `Welcome, John. You are ${age} years old.`
+      };
+    });
   }
   
-  const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (age >= 18) {
-        resolve();
-      } else {
-        reject();
-      }
-    }, 4000);
-  })
-  .then(() => {
-    alert(`Welcome, ${name}. You can vote.`);
-  })
-  .catch(() => {
-    alert(`Oh sorry ${name}. You aren't old enough.`);
+  myPromise().then(data => {
+    alert(data.welcomeMessage);
   });
-});
-
-
-
-
-
